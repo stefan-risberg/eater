2a387e3b4b04717cda9d2a00582638a99a27c372
Stefan Risberg <steffenomak@gmail.com>
2014-03-15 15:59:18 +0100

    Added documentation.

--- inc/eater/nutrients.hpp
+++ inc/eater/nutrients.hpp
@@ -15,10 +15,10 @@ namespace Eater {
             /**
              * Default constructor.
              *
-             * \param[in] _calories Calorie count for 100g.
-             * \param[in] _proteins Protein count for 100g.
-             * \param[in] _carbohydrates Carbohydrat count for 100g.
-             * \param[in] _fats Fat count for 100g.
+             * \param _calories Calorie count for 100g.
+             * \param _proteins Protein count for 100g.
+             * \param _carbohydrates Carbohydrat count for 100g.
+             * \param _fats Fat count for 100g.
              */
             Nutrients(f32 _calories = 0,
                       f32 _proteins = 0.0,
@@ -28,28 +28,57 @@ namespace Eater {
             /**
              * Set calorie count.
              *
-             * \param[in] _calories New calorie count.
+             * \param _calories New calorie count.
              */
             void calories(f32 _calories);
 
             /**
              * Set protein count.
              *
-             * \param[in] _protins New protein count.
+             * \param _protins New protein count.
              */
             void proteins(f32 _proteins);
 
             /**
              * Set carbohydrate count.
              *
-             * \param[in] _carbohydrates New carbohydrate count.
+             * \param  _carbohydrates New carbohydrate count.
              */
             void carbohydrates(f32 _carbohydrates);
+
+            /**
+             * Set fat count.
+             *
+             * \param _fats New fat count.
+             */
             void fats(f32 _fats);
 
+            /**
+             * Get calorie count.
+             *
+             * \return Calorie count.
+             */
             f32 calories() const;
+
+            /**
+             * Get protein count.
+             *
+             * \return Protein count.
+             */
             f32 proteins() const;
+
+            /**
+             * Get carbohydrate count.
+             *
+             * \return Carbohydrate count.
+             */
             f32 carbohydrates() const;
+
+            /**
+             * Get fat count.
+             *
+             * \return Fat count.
+             */
             f32 fats() const;
     };
 }
