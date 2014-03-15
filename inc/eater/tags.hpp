138fcdc2896328648b6c59d9fae8ac245b3aad6e
Stefan Risberg <steffenomak@gmail.com>
2014-03-15 19:30:49 +0100

    Added tags.

---
+++ inc/eater/tags.hpp
@@ -0,0 +1,117 @@
+#ifndef TAGS_HPP_
+#define TAGS_HPP_
+
+#include "eater/common.hpp"
+
+namespace Eater
+{
+    /**
+     * Holds only unique tags.
+     */
+    class Tags
+    {
+    private:
+        tags_t *_tags = nullptr;
+
+    public:
+        /**
+         * Takes ownership of a tag vector.
+         *
+         * \param[in] tags Vector of tags.
+         * \param valid True if tags doesn't contain duplicates.  
+         */
+        Tags(tags_t *tags, bool valid = true);
+
+        /**
+         * Default constructor.
+         */
+        Tags();
+
+        /**
+         * Copy constructor.
+         *
+         * \param tags To copy. 
+         */
+        Tags(const Tags &tags);
+
+        /**
+         * Default destructor.
+         */
+        ~Tags();
+
+        /**
+         * Adds a tag.
+         *
+         * Will not add duplicated.
+         *
+         * \param tag Tag to add.
+         * \return If tag exists, false. Else true. 
+         */
+        bool addTag(const std::string &tag);
+
+        /**
+         * Adds tags.
+         *
+         * Will not add duplicates.
+         *
+         * \param tags Tags to be added.
+         * \return If one, some or all are already presant, false. Else true. 
+         */
+        bool addTags(const tags_t &tags);
+
+        /**
+         * Removes a tag.
+         *
+         * \param tag Tag to remove
+         * \return Ture if tag was removed, else false. 
+         */
+        bool removeTag(const std::string &tag);
+
+        /**
+         * Removes a tag.
+         *
+         * \param it Iterator position.
+         * \return Iterator following the last removed element.
+         */
+        tags_t::iterator removeTag(tags_t::iterator &it);
+
+        /**
+         * Removes tags.
+         *
+         * \param tags Tags to be removed.
+         * \return false if not all tags was removed, else false. 
+         */
+        bool removeTags(const tags_t &tags);
+
+        /**
+         * Gets an iterator.
+         *
+         * \return Gets an iterator at the beginning of tags.
+         */
+        tags_t::iterator begin();
+
+        /**
+         * Gets an iterator.
+         *
+         * \return Gets an iterator at the beginning of tags.
+         */
+        tags_t::const_iterator begin() const;
+        
+        /**
+         * Gets an iterator.
+         *
+         * \return Gets an interator at the end of the tags.
+         */
+        tags_t::iterator end();
+
+        /**
+         * Gets an iterator.
+         *
+         * \return Gets an interator at the end of the tags.
+         */
+        tags_t::const_iterator end() const;
+    };
+} /* Eater */ 
+
+#endif /* end of include guard: TAGS_HPP_ */
+
---
+++ src/tags.cpp
@@ -0,0 +1,119 @@
+#include "eater/tags.hpp"
+
+namespace Eater
+{
+    Tags::Tags(tags_t *tags, bool valid)
+    {
+        if (!valid) {
+            for (size_t i = 0; i < tags->size(); i++) {
+                for (size_t j = i + 1; j < tags->size(); j++) {
+                    if (tags->at(i) == tags->at(j)) {
+                        tags->erase(tags->begin() + j);
+                        j--;
+                    }
+                }
+            }
+        }
+
+        _tags = tags;
+    }
+
+    Tags::Tags()
+    {
+        _tags = new tags_t;
+    }
+
+    Tags::Tags(const Tags &tags)
+    {
+        _tags = new tags_t;
+        for (auto it = tags.begin(); it != tags.end(); it++) {
+            _tags->push_back(*it);
+        }
+    }
+
+    Tags::~Tags()
+    {
+        if (_tags != nullptr) {
+            delete _tags;
+        }
+    }
+
+    bool Tags::addTag(const std::string &tag)
+    {
+        for (auto it = begin(); it != end(); it++) {
+            if (*it == tag) {
+                return false;
+            }
+        }
+
+        _tags->push_back(tag);
+        return true;
+    }
+
+    bool Tags::addTags(const tags_t &tags)
+    {
+        bool found_dup = false;
+
+        for (auto it = tags.begin(); it != tags.end(); it++) {
+            bool r = addTag(*it);
+
+            if (!r) {
+                found_dup = true;
+            }
+        }
+
+        return !found_dup;
+    }
+
+    bool Tags::removeTag(const std::string &tag)
+    {
+        for (auto it = begin(); it != end(); it++) {
+            if (*it == tag) {
+                _tags->erase(it);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    tags_t::iterator Tags::removeTag(tags_t::iterator &it)
+    {
+        return _tags->erase(it);
+    }
+
+    bool Tags::removeTags(const tags_t &tags)
+    {
+        bool one_failed = false;
+
+        for (auto it = tags.begin(); it != tags.end(); it++) {
+            bool r = removeTag(*it);
+
+            if (!r) {
+                one_failed = true;
+            }
+        }
+
+        return !one_failed;
+    }
+
+    tags_t::iterator Tags::begin()
+    {
+        return _tags->begin();
+    }
+
+    tags_t::const_iterator Tags::begin() const
+    {
+        return _tags->cbegin();
+    }
+
+    tags_t::iterator Tags::end()
+    {
+        return _tags->end();
+    }
+
+    tags_t::const_iterator Tags::end() const
+    {
+        return _tags->cend();
+    }
+} /* Eater */ 
