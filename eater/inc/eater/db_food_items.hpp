d05b51f8a104fd997d432f95056dc6b93f7b0a5f
Stefan Risberg <steffenomak@gmail.com>
2014-03-16 20:13:08 +0100

    Added DB_FoodItems

--- eater/inc/eater/db_food_items.hpp
+++ eater/inc/eater/db_food_items.hpp
@@ -9,17 +9,98 @@
 
 namespace Eater
 {
-    typedef std::vector<FoodItem> food_item_vec;
-
     class DB_FoodItems
     {
     private:
         sqlite3 *db;
 
     public:
+        /**
+         * Default constructor.
+         *
+         * Does nothing.
+         */
         DB_FoodItems();
-        bool open(const std::string &db);
-        bool close();
+
+        /**
+         * Opens a connection to food item database.
+         *
+         * If not found one will be created and initialized.
+         *
+         * \param _db Name of database.
+         * \return true if opening or creation of database was succesfull,
+         * else false. 
+         */
+        bool open(const std::string &_db);
+
+        /**
+         * Closes the database.
+         */
+        void close();
+
+        /**
+         * Check if a certain food item exists in database.
+         *
+         * \param item Item to check if it exists.
+         * \return true if it does, else false. 
+         */
+        bool exists(const id_t item) const;
+
+        /**
+         * Check if the item is older then the one in the database.
+         *
+         * \param item Item to check.
+         * \return true if is older, false if it is not odler or if the
+         * item was not found in the database. 
+         */
+        bool old(const FoodItem &item) const;
+
+        /**
+         * Updates a given item in the database.
+         *
+         * Updates the item if it exists, will not check if item is older
+         * then the one in the database or if it exists in the database.
+         *
+         * \param item Item to update.
+         */
+        void update(const FoodItem &item);
+
+        /**
+         * Saves an item.
+         *
+         * If item id field is anything then -1 saving will fail. The id will be
+         * updated for the item if saving was successfull.
+         *
+         * \param item Item to save.
+         */
+        void save(FoodItem &item);
+
+        /**
+         * Searches for an item by id in the database.
+         *
+         * \param id Id to look for.
+         * \param item Item will be overwritten if item was found.
+         * \return true if found else false.  
+         */
+        bool find(const id_t id, FoodItem &item) const;
+
+    private:
+        /**
+         * Initializes the database.
+         *
+         * If the database already exists and the correct table exits then
+         * nothing else will happen.
+         *
+         * \return true if database is ready to use, else false.
+         */
+        bool init();
+
+        /**
+         * Checks if FOODITEMS table exits in the database.
+         *
+         * \return true if it does else false.
+         */
+        bool tableExists() const;
 
     };
 } /* Eater */ 
--- eater/src/db_food_items.cpp
+++ eater/src/db_food_items.cpp
@@ -1,27 +1,331 @@
 #include "eater/db_food_items.hpp"
+#include <fstream>
+#include <cstring>
 
 namespace Eater
 {
     DB_FoodItems::DB_FoodItems()
     {}
 
-    bool DB_FoodItems::open(const std::string &db)
+    bool DB_FoodItems::open(const std::string &_db)
     {
-        auto rc = sqlite3_open(db.c_str(), &(this->db));
+        db = nullptr;
+        auto rc = sqlite3_open(_db.c_str(), &db);
+
+        if (db == nullptr) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("db is null\n");
+            return false;
+        }
 
         if (rc) {
-            LOGG(E_RED("Can't open database: "));
-            LOGG(sqlite3_errmsg(this->db));
-            LOGG("\n");
+            LOGG(E_RED("ERROR: "));
+            LOGG("Can't open database - ");
+            LOGG_LN(sqlite3_errmsg(this->db));
             return false;
         } 
+
+        if (!init()) {
+            return false;
+        }
+
+        LOGG("Opened database: ");
+        LOGG_LN(_db);
         return true;
     }
 
-    bool DB_FoodItems::close()
+    void DB_FoodItems::close()
     {
-        sqlite3_close(db);
+        if (db != nullptr) {
+            sqlite3_close(db);
+            db = nullptr;
+        }
+    }
+
+    bool DB_FoodItems::exists(const id_t item) const
+    {
+        std::stringstream ss;
+        ss << "select id from fooditems where id=" << item << ";";
+
+        sqlite3_stmt *s;
+        int r = sqlite3_prepare_v2(db, ss.str().c_str(),
+                                   -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+            return false;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r == SQLITE_DONE) {
+            sqlite3_finalize(s);
+            return false;
+        } else if (r == SQLITE_ROW) {
+            r = sqlite3_column_int(s, 0);
+            sqlite3_finalize(s);
+
+            if (r == item) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool DB_FoodItems::old(const FoodItem &item) const
+    {
+        std::stringstream ss;
+        ss << "select date,time from fooditems where id=" << item.id() << ";";
+
+        sqlite3_stmt *s;
+
+        int r = sqlite3_prepare_v2(db, ss.str().c_str(), -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return false;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r == SQLITE_DONE) {
+            sqlite3_finalize(s);
+
+            return false;
+        } else if (r == SQLITE_ROW) {
+            TimeStamp ts(sqlite3_column_int(s, 1),
+                         sqlite3_column_int(s, 0));
+
+            sqlite3_finalize(s);
+            if (item.time_stamp > ts) {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    void DB_FoodItems::update(const FoodItem &item)
+    {
+        std::stringstream ss;
+        ss << "update fooditems set "
+            << "date=" << item.time_stamp.getDate() << ", "
+            << "time=" << item.time_stamp.getTime() << ", "
+            << "name='" << item.name() << "', "
+            << "brand='" << item.brand() << "', "
+            << "tags='" << item.tags.toString() << "', "
+            << "kcal=" << item.macro_nutrients.calories() << ", "
+            << "proteins=" << item.macro_nutrients.proteins() << ", "
+            << "carbohydrates=" << item.macro_nutrients.carbohydrates() << ", "
+            << "fats=" << item.macro_nutrients.fats()
+            << " where id=" << item.id() << ";";
+
+        sqlite3_stmt *s;
+        int r = sqlite3_prepare_v2(db, ss.str().c_str(), -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("update: Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r != SQLITE_DONE) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Fiald to update, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+        }
+
+        sqlite3_finalize(s);
+    }
+
+    void DB_FoodItems::save(FoodItem &item)
+    {
+        std::stringstream ss;
+        ss << "insert into fooditems (date, time, name, brand, tags, "
+            << "kcal, proteins, carbohydrates, fats) "
+            << "values ("
+            << item.time_stamp.getDate() << ", "
+            << item.time_stamp.getTime() << ", "
+            << "'" << item.name() << "', "
+            << "'" << item.brand() << "', "
+            << "'" << item.tags.toString() << "', "
+            << item.macro_nutrients.calories() << ", "
+            << item.macro_nutrients.proteins() << ", "
+            << item.macro_nutrients.carbohydrates() << ", "
+            << item.macro_nutrients.fats() << ");";
+
+        std::cout << ss.str() << std::endl;
+        sqlite3_stmt *s;
+        int r = sqlite3_prepare_v2(db, ss.str().c_str(), -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("save: Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r != SQLITE_DONE) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Fiald to update, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+        } else {
+            item.id(sqlite3_last_insert_rowid(db));
+        }
+
+        sqlite3_finalize(s);
+    }
+
+    bool DB_FoodItems::find(const id_t id, FoodItem &item) const
+    {
+        std::stringstream ss;
+        ss << "select * from fooditems where id=" << id << ";";
+
+        sqlite3_stmt *s;
+        int r = sqlite3_prepare_v2(db, ss.str().c_str(), -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("find: Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return false;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r == SQLITE_DONE) {
+            sqlite3_finalize(s);
+            return false;
+        } else if (r == SQLITE_ROW) {
+            item.id(sqlite3_column_int(s, 0));
+            item.time_stamp.setDate(sqlite3_column_int(s, 1));
+            item.time_stamp.setTime(sqlite3_column_int(s, 2));
+            
+            ss.str(std::string()); ss << sqlite3_column_text(s, 3);
+            item.name(ss.str());
+            ss.str(std::string()); ss << sqlite3_column_text(s, 4);
+            item.brand(ss.str());
+            ss.str(std::string()); ss << sqlite3_column_text(s, 5);
+            item.tags.fromString(ss.str());
+            item.macro_nutrients.calories(sqlite3_column_double(s, 6));
+            item.macro_nutrients.proteins(sqlite3_column_double(s, 7));
+            item.macro_nutrients.carbohydrates(sqlite3_column_double(s, 8));
+            item.macro_nutrients.fats(sqlite3_column_double(s, 9));
+
+            sqlite3_finalize(s);
+
+            return true;
+        }
+        LOGG(E_RED("ERROR: "));
+        LOGG("Something in find didn't pan out, return code: ");
+        LOGG_LN(E_MAGENTA(r));
+
+        sqlite3_finalize(s);
+
+        return false;
+    }
+
+    bool DB_FoodItems::init()
+    {
+        if (db == nullptr) {
+            return false;
+        }
+
+        if (tableExists()) {
+            return true;
+        }
+
+        std::string sql = "create table fooditems("\
+                          "id integer primary key,"\
+                          "date integer not null,"\
+                          "time integer not null,"\
+                          "name text not null,"\
+                          "brand text not null,"\
+                          "tags text,"\
+                          "kcal real,"\
+                          "proteins real,"\
+                          "carbohydrates real,"\
+                          "fats real"\
+                          ");";
+
+        sqlite3_stmt *statement;
+
+        auto r = sqlite3_prepare_v2(db, sql.c_str(), -1, &statement, nullptr);
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Faild to prepare statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return false;
+        }
+
+        r = sqlite3_step(statement);
+
+        if (r != SQLITE_DONE) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Faild to step statement, return code: ");
+            LOGG_LN(E_MAGENTA(r));
+
+            return false;
+        }
+
+        sqlite3_finalize(statement);
+        LOGG_LN("Init of database table sucessfull");
+
+        return true;
+    }
+
+    bool DB_FoodItems::tableExists() const
+    {
+        std::string sql = "select count(*) from sqlite_master "\
+                          "where type='table' and name='fooditems';";
+
+        sqlite3_stmt *s;
+
+        auto r = sqlite3_prepare_v2(db , sql.c_str(), -1, &s, nullptr);
+
+        if (r != SQLITE_OK) {
+            LOGG(E_RED("ERROR: "));
+            LOGG("Something is wrong: ");
+            LOGG_LN(E_MAGENTA(r));
+            return false;
+        }
+
+        r = sqlite3_step(s);
+
+        if (r == SQLITE_DONE) {
+            sqlite3_finalize(s);
+            LOGG_LN("No fooditems table found");
+            return false;
+        }
+
+        if (r == SQLITE_ROW) {
+            r = sqlite3_column_int(s, 0);
+
+            sqlite3_finalize(s);
+            if (r == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        sqlite3_finalize(s);
+        return false;
     }
 
 } /* Eater */ 
