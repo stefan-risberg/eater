5bfa0eae6bc74dcfea9f26f92010295e4039ac28
Stefan Risberg <steffenomak@gmail.com>
2014-04-09 23:40:45 +0200

    Update 2.0

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -58,12 +58,9 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-'-isystem',
-'./eater/inc',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
+'-I./inc',
+'-I/usr/include',
+'-I/usr/local/include',
 '-lsqlite3',
 ]
 
