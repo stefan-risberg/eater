5c7a77742476a14362203227d62a57c7aa2bcc1e
Stefan Risberg <steffenomak@gmail.com>
2014-08-12 12:39:16 +0200

    Fixed debug printout.

--- src/db_tags.cpp
+++ src/db_tags.cpp
@@ -16,18 +16,7 @@ const char *DB_Tags::col_tags_id = "tag_id";
 const char *DB_Tags::col_tags_name = "name";
 const char *DB_Tags::col_fooditem_id = "fooditem_id";
 
-DB_Tags::DB_Tags(std::shared_ptr<DB_Driver> &db) : TableHandler(db)
-{
-}
-
-DB_Tags::~DB_Tags()
-{
-    if (!close()) {
-        LOGG_LN("Well this should not happen....");
-    }
-}
-
-bool DB_Tags::init()
+bool DB_Tags::initTagsTable()
 {
     if (db->tableExists(tbl_tags)) {
         LOGG_MESSAGE(tbl_tags << " exists.");
@@ -45,14 +34,17 @@ bool DB_Tags::init()
         LOGG_MESSAGE("Creating table " << tbl_tags << ".");
 
         if (!db->createTable(tbl_tags, col_names, col_types)) {
-            LOGG_ERROR(__PRETTY_FUNCTION__ << " faild to create "
-                                           << tbl_tags << ".");
+            LOGG_ERROR(__PRETTY_FUNCTION__);
 
             return false;
         }
     }
 
+    return true;
+}
 
+bool DB_Tags::initFooditemTagsTable()
+{
     if (db->tableExists(tbl_fooditem_tags)) {
         LOGG_MESSAGE(tbl_fooditem_tags << " exists.");
     } else {
@@ -71,11 +63,32 @@ bool DB_Tags::init()
         LOGG_MESSAGE("Try to create table " << tbl_fooditem_tags << ".");
 
         if (!db->createTable(tbl_fooditem_tags, col_names, col_types)) {
-            LOGG_ERROR(__PRETTY_FUNCTION__ << " - Failed to create table "
-                                           << tbl_fooditem_tags << ".");
+            LOGG_ERROR(__PRETTY_FUNCTION__);
 
             return false;
         }
+    }
+
+    return true;
+}
+
+DB_Tags::DB_Tags(std::shared_ptr<DB_Driver> &db) : TableHandler(db)
+{
+}
+
+DB_Tags::~DB_Tags()
+{
+    if (!close()) {
+        LOGG_LN("Well this should not happen....");
+    }
+}
+
+bool DB_Tags::init()
+{
+    if (!initTagsTable() && !initFooditemTagsTable()) {
+        LOGG_ERROR(__PRETTY_FUNCTION__);
+
+        return false;
     }
 
     return true;
