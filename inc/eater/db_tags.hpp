e47ce4a466ea7419e1dbace09403a63522348a78
Stefan Risberg <steffenomak@gmail.com>
2014-08-12 12:32:20 +0200

    Inital tags database implementatino.
    Implemented the init method.

--- inc/eater/db_tags.hpp
+++ inc/eater/db_tags.hpp
@@ -4,31 +4,29 @@
 #include "eater/common.hpp"
 #include "eater/tags.hpp"
 #include "eater/table_handler.hpp"
+#include "eater/db_driver.hpp"
+#include <memory>
 
 namespace Eater
 {
 class DB_Tags : TableHandler
 {
    private:
-    // Tags table fields.
-    static const char *tb_tags;     /*!< Table name. */
-    static const char *c_tags_text; /*!< Tags name column. */
+    static const char *col_id;
 
-    static const char *tb_food_tags;
-    static const char *c_foods_id;
-    static const char *c_tags_id;
+    static const char *tbl_tags;
+    static const char *tbl_fooditem_tags;
 
-    static const char *c_id;
+    static const char *col_tags_id;
+    static const char *col_tags_name;
+    static const char *col_fooditem_id;
 
    public:
-    DB_Tags(shared_sqlite3 &db);
+    DB_Tags(std::shared_ptr<DB_Driver> &db);
     virtual ~DB_Tags();
 
     virtual bool init();
-    virtual bool close()
-    {
-        return true;
-    }
+    virtual bool close();
 };
 } /* Eater */
 
--- src/db_tags.cpp
+++ src/db_tags.cpp
@@ -7,14 +7,16 @@
 
 namespace Eater
 {
-const char *DB_Tags::tb_tags = "tags";
-const char *DB_Tags::c_tags_text = "text";
-const char *DB_Tags::tb_food_tags = "food_tags";
-const char *DB_Tags::c_foods_id = "food_id";
-const char *DB_Tags::c_tags_id = "tags_id";
-const char *DB_Tags::c_id = "id";
+const char *DB_Tags::col_id = "id";
 
-DB_Tags::DB_Tags(shared_sqlite3 &db) : TableHandler(db)
+const char *DB_Tags::tbl_tags = "tags";
+const char *DB_Tags::tbl_fooditem_tags = "fooditem_tags";
+
+const char *DB_Tags::col_tags_id = "tag_id";
+const char *DB_Tags::col_tags_name = "name";
+const char *DB_Tags::col_fooditem_id = "fooditem_id";
+
+DB_Tags::DB_Tags(std::shared_ptr<DB_Driver> &db) : TableHandler(db)
 {
 }
 
@@ -27,28 +29,60 @@ DB_Tags::~DB_Tags()
 
 bool DB_Tags::init()
 {
-    if (!DB::tableExists(db, tb_tags)) {
-        std::vector<std::string> col_names = {c_id, c_tags_text};
-
-        std::vector<std::string> col_types = {"integer primary key",
-                                              "text not null"};
-
-        if (!DB::createTable(db, tb_tags, col_names, col_types)) {
-            return false;
-        }
-    }
-
-    if (!DB::tableExists(db, tb_food_tags)) {
-        std::vector<std::string> col_names = {c_id, c_tags_id, c_foods_id};
+    if (db->tableExists(tbl_tags)) {
+        LOGG_MESSAGE(tbl_tags << " exists.");
+    } else {
+        std::vector<std::string> col_names = {
+            col_id,
+            col_tags_name
+        };
 
         std::vector<std::string> col_types = {
-            "integer primary key", "integer not null", "integer not null"};
+            "integer primary key",
+            "text not null"
+        };
 
-        if (!DB::createTable(db, tb_food_tags, col_names, col_types)) {
+        LOGG_MESSAGE("Creating table " << tbl_tags << ".");
+
+        if (!db->createTable(tbl_tags, col_names, col_types)) {
+            LOGG_ERROR(__PRETTY_FUNCTION__ << " faild to create "
+                                           << tbl_tags << ".");
+
             return false;
         }
     }
 
+
+    if (db->tableExists(tbl_fooditem_tags)) {
+        LOGG_MESSAGE(tbl_fooditem_tags << " exists.");
+    } else {
+        std::vector<std::string> col_names = {
+            col_id,
+            col_tags_id,
+            col_fooditem_id
+        };
+
+        std::vector<std::string> col_types = {
+            "integer primary key",
+            "integer not null",
+            "integer not null"
+        };
+
+        LOGG_MESSAGE("Try to create table " << tbl_fooditem_tags << ".");
+
+        if (!db->createTable(tbl_fooditem_tags, col_names, col_types)) {
+            LOGG_ERROR(__PRETTY_FUNCTION__ << " - Failed to create table "
+                                           << tbl_fooditem_tags << ".");
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool DB_Tags::close()
+{
     return true;
 }
 } /* Eater */
