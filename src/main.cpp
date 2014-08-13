b2dd71b14a882f6492800fe579b09b032a5271f1
Stefan Risberg <steffenomak@gmail.com>
2014-08-14 00:54:28 +0200

    Added some tests.

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,92 @@
-#include <iostream>
 #include "eater/common.hpp"
-#include "eater/fooditem.hpp"
-#include "eater/db.hpp"
-#include "format.h"
-#include "eater/sql.hpp"
-#include <vector>
+#include "eater/date.hpp"
+#include <iostream>
 #include <string>
+
+#define DO_TEST
+
+#ifdef DO_TEST
+void doTest();
+
+inline bool test(int n,
+                 bool cond);
+inline void printHeader(const std::string &header);
+
+#endif //DO_TEST
 
 int main()
 {
-    Eater::DB db;
-    db.open("test.db");
 
-    Eater::food_vec vec;
-    Eater::FoodItem it;
-    it.name("fisk");
-    it.brand("abba");
-    it.mn.calories(89);
-    it.mn.carbohydrates(4);
-    it.mn.proteins(19);
-    it.mn.fats(2);
-    it.ts.setCurrent();
-    it.tags.addTags({"fisk", "hav", "ekologisk"});
-
-    std::cout << "\n\n" << it.tags.toString() << "\n\n";
-
-    vec.push_back(it);
-
-    it.name("crabba");
-    it.mn.calories(101);
-    it.mn.proteins(21);
-    it.ts.setCurrent();
-    it.tags.removeTag("fisk");
-    it.tags.addTag("skaldjur");
-
-    vec.push_back(it);
-
-    LOGG_MESSAGE("test3");
-
-    db.food_items->save(vec);
-
-    for (auto i : vec) {
-        std::cout << i.name() << " " << i.id() << std::endl;
-    }
-
-    /*std::shared_ptr<Eater::DB_Driver> db(new Eater::Sql());
-    db->open("test.sql");
-
-    if (!db->tableExists("apa")) {
-        LOGG_MESSAGE("Table doesn't exist");
-        db->createTable("apa", {"id", "name"}, {"integer primary key", "text"});
-    }
-
-    if (!db->insert("apa", "name", "\"fisk\"")) {
-        LOGG_ERROR("Faild to insert data.");
-        return 1;
-    }
-
-    //auto last_id = s.getLastInsertRowId();
-
-    if (!db->update("apa", "name=\"grek\"", "id=2")) {
-        LOGG_ERROR("Faild to update data.");
-        return 2;
-    }
-
-
-    //std::cout << s.getLastInsertRowId() << std::endl;
-    */
+#ifdef DO_TEST
+    doTest();
+#endif //DO_TEST
 
     return 0;
 }
+
+#ifdef DO_TEST
+void doTest()
+{
+    // Date tests.
+    printHeader("Date tests.");
+
+    {
+        Eater::Date d(2012, 1, 2);
+
+        test(1, d.year() == 2012);
+        test(2, d.month() == 1);
+        test(3, d.day() == 2);
+
+        d.year(2014);
+        d.month(3);
+        d.day(4);
+
+        test(4, d.year() == 2014);
+        test(5, d.month() == 3);
+        test(6, d.day() == 4);
+
+        Eater::Date d1(d.getDate());
+
+        test(7, d1.year() == 2014);
+        test(8, d1.month() == 3);
+        test(9, d1.day() == 2);
+    }
+}
+
+bool test(int n,
+          bool cond)
+{
+    if (!cond) {
+        std::cout
+            << E_RED("Failed    ")
+            << " test: "
+            << n
+            << std::endl;
+
+        return false;
+    } else {
+        std::cout
+            << "Successful test: "
+            << n
+            << std::endl;
+
+        return true;
+    }
+}
+
+void printHeader(const std::string &header)
+{
+    for (int i = 0; i < 8; i++) {
+        std::cout << "**********";
+    }
+
+    std::cout << std::endl << "** " << header << std::endl;
+
+    for (int i = 0; i < 8; i++) {
+        std::cout << "**********";
+    }
+
+    std::cout << std::endl;
+}
+
+#endif //DO_TEST
